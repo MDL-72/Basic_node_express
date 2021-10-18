c8010ae0ecfcd1822908ad073ff8f05d7dc56afe
MDL-72 <91296613+MDL-72@users.noreply.github.com>
2021-10-18 07:54:44 +0000

    app get try

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 
-
+app.get("/", function(req, res) {
+res.send('Hello Express');
+});
+app.listen(3001, function() {
+console.log('Listening on port 3000');
+});
 
 console.log("Hello World")
 
