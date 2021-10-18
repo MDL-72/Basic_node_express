25af80db8f6129281aea4cf253e3de8f4867dd18
MDL-72 <91296613+MDL-72@users.noreply.github.com>
2021-10-18 09:16:52 +0000

    Serving JSON on specific route

--- myApp.js
+++ myApp.js
@@ -12,7 +12,21 @@ console.log("Hello World")
 
 
 
+//getting html from the dir
+app.get("/", function(req, res) {
+res.sendFile(__dirname + "/views/index.html");
+});
 
+//getting the css style from the dir
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+//Serving JSON on specific route
+app.get("/json", function(req, res) {
+  res.json(
+    {"message": "Hello json"}
+    );
+});
 
 
 
