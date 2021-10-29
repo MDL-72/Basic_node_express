67eceea3f7465d6194ee5eb46681aa7ea2f763b7
MDL-72 <91296613+MDL-72@users.noreply.github.com>
2021-10-29 23:55:16 +0000

    placing root level middleware logger

--- myApp.js
+++ myApp.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
-require('dotenv').config()
+
 
 console.log("Hello World")
 
 
 // asdfsdfsdf
 
+app.use((req, res, next) => {
+  let method = req.method;
+  let path = req.path;
+  let ip = req.ip;
 
+  console.log(method + " " +path + " - " +ip);
+  next();
+});
 
 
 
@@ -21,29 +28,18 @@ res.sendFile(__dirname + "/views/index.html");
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
+
+
 //Serving JSON on specific route
-//app.get("/json", function(req, res) {
-  //res.json(
-    //{"message": process.env.MESSAGE_STYLE == "uppercase" ? "HELLO JSON": "Hello json"}
-    //);
-//});
-
-//configuring .env
-
-app.get("/json", function(req,res){
-
-  console.log(process.env.MESSAGE_STYLE, " <= message style");
-  if(process.env.MESSAGE_STYLE === "uppercase"){
-    res.json({
-      "message" : "HELLO JSON"
-    })
-  }
-  else{
-    res.json({
-      "message" : "Hello json"
-    })
-  }
+app.get("/json", function(req, res) {
+  res.json(
+    {"message": "Hello json"}
+    );
 });
+
+// 
+
+
 
 
 
